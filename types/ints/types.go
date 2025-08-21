540d3c4a9fee72cec54dce9c71c8710767fafb26
Tomáš Novotný <tomas.novotny.301@gmail.com>
2025-08-21 14:44:56 +0200

    Init integer and float geometry type aliases

---
+++ types/ints/types.go
@@ -0,0 +1,11 @@
+package ints
+
+import geom "github.com/gravitton/geometry"
+
+type Circle = geom.Circle[int]
+type Line = geom.Line[int]
+type Point = geom.Point[int]
+type Polygon = geom.Polygon[int]
+type Rectangle = geom.Rectangle[int]
+type Size = geom.Size[int]
+type Vector = geom.Vector[int]
