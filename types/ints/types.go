fdca5c3a331f4e5320d97f93b0924c0ce16b991c
Tomáš Novotný <tomas.novotny.301@gmail.com>
2025-08-24 15:22:36 +0200

    Fix go vet

--- types/ints/types.go
+++ types/ints/types.go
@@ -12,35 +12,35 @@ type Polygon = geom.Polygon[int]
 type RegularPolygon = geom.RegularPolygon[int]
 
 func ToPoint[T geom.Number](point geom.Point[T]) Point {
-	return Point{geom.Cast[int](float64(point.X)), geom.Cast[int](float64(point.Y))}
+	return Point{X: geom.Cast[int](float64(point.X)), Y: geom.Cast[int](float64(point.Y))}
 }
 
 func ToVector[T geom.Number](vector geom.Vector[T]) Vector {
-	return Vector{geom.Cast[int](float64(vector.X)), geom.Cast[int](float64(vector.Y))}
+	return Vector{X: geom.Cast[int](float64(vector.X)), Y: geom.Cast[int](float64(vector.Y))}
 }
 
 func ToSize[T geom.Number](size geom.Size[T]) Size {
-	return Size{geom.Cast[int](float64(size.Width)), geom.Cast[int](float64(size.Height))}
+	return Size{Width: geom.Cast[int](float64(size.Width)), Height: geom.Cast[int](float64(size.Height))}
 }
 
 func ToCircle[T geom.Number](circle geom.Circle[T]) Circle {
-	return Circle{ToPoint(circle.Center), geom.Cast[int](float64(circle.Radius))}
+	return Circle{Center: ToPoint(circle.Center), Radius: geom.Cast[int](float64(circle.Radius))}
 }
 
 func ToLine[T geom.Number](line geom.Line[T]) Line {
-	return Line{ToPoint(line.Start), ToPoint(line.End)}
+	return Line{Start: ToPoint(line.Start), End: ToPoint(line.End)}
 }
 
 func ToRectangle[T geom.Number](rectangle geom.Rectangle[T]) Rectangle {
-	return Rectangle{ToPoint(rectangle.Center), ToSize(rectangle.Size)}
+	return Rectangle{Center: ToPoint(rectangle.Center), Size: ToSize(rectangle.Size)}
 }
 
 func ToPolygon[T geom.Number](polygon geom.Polygon[T]) Polygon {
-	return Polygon{geom.Transform(polygon.Vertices, func(point geom.Point[T]) Point {
+	return Polygon{Vertices: geom.Transform(polygon.Vertices, func(point geom.Point[T]) Point {
 		return ToPoint(point)
 	})}
 }
 
 func ToRegularPolygon[T geom.Number](polygon geom.RegularPolygon[T]) RegularPolygon {
-	return RegularPolygon{ToPoint(polygon.Center), ToSize(polygon.Size), polygon.N}
+	return RegularPolygon{Center: ToPoint(polygon.Center), Size: ToSize(polygon.Size), N: polygon.N}
 }
