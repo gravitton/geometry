80f4f0b9742302a0f591d692bb372faa3e464365
Tomáš Novotný <tomas.novotny.301@gmail.com>
2025-08-24 13:23:16 +0200

    Rename `Pt` to `P` and add more `Point` methods for operations

--- point.go
+++ point.go
@@ -10,44 +10,74 @@ type Point[T Number] struct {
 	Y T `json:"y"`
 }
 
-// Pt is shorthand for Point[T]{x, y}.
-func Pt[T Number](x, y T) Point[T] {
+// P is shorthand for Point[T]{x, y}.
+func P[T Number](x, y T) Point[T] {
 	return Point[T]{x, y}
 }
 
+// XY returns the point X, Y values in standard order.
+func (p Point[T]) XY() (T, T) {
+	return p.X, p.Y
+}
+
 // Add creates a new Point by adding the given vector to the current point.
-func (p Point[T]) Add(change Vector[T]) Point[T] {
-	return Point[T]{p.X + change.X, p.Y + change.Y}
+func (p Point[T]) Add(v Vector[T]) Point[T] {
+	return Point[T]{p.X + v.X, p.Y + v.Y}
 }
 
-// Sub creates a new Vector by subtracting the given point from the current point.
-func (p Point[T]) Sub(other Point[T]) Vector[T] {
-	return Vector[T]{p.X - other.X, p.Y - other.Y}
+// AddXY creates a new Point by adding the given values to the current point.
+func (p Point[T]) AddXY(dx, dy T) Point[T] {
+	return Point[T]{p.X + dx, p.Y + dy}
 }
 
-// Midpoint creates a new Point between current and given points
+// Subtract creates a new Vector from given point to current point.
+func (p Point[T]) Subtract(o Point[T]) Vector[T] {
+	return Vector[T]{p.X - o.X, p.Y - o.Y}
+}
+
+// Multiply creates a new Point by multiplying the given value to the current point.
+func (p Point[T]) Multiply(s float64) Point[T] {
+	return Point[T]{Multiple(p.X, s), Multiple(p.Y, s)}
+}
+
+// MultiplyXY creates a new Point by multiplying the given values to the current point.
+func (p Point[T]) MultiplyXY(sx, sy float64) Point[T] {
+	return Point[T]{Multiple(p.X, sx), Multiple(p.Y, sy)}
+}
+
+// Divide creates a new Point by dividing the given value to the current point.
+func (p Point[T]) Divide(s float64) Point[T] {
+	return Point[T]{Divide(p.X, s), Divide(p.Y, s)}
+}
+
+// DivideXY creates a new Point by dividing the given values to the current point.
+func (p Point[T]) DivideXY(sx, sy float64) Point[T] {
+	return Point[T]{Divide(p.X, sx), Divide(p.Y, sy)}
+}
+
+// Midpoint creates a new Point between current and given points.
 func (p Point[T]) Midpoint(other Point[T]) Point[T] {
 	return Point[T]{Midpoint(p.X, other.X), Midpoint(p.Y, other.Y)}
 }
 
-// Lerp creates a new Point in linear interpolation towards given point
+// Lerp creates a new Point in linear interpolation towards given point.
 func (p Point[T]) Lerp(other Point[T], t float64) Point[T] {
 	return Point[T]{Lerp(p.X, other.X, t), Lerp(p.Y, other.Y, t)}
 }
 
 // DistanceTo return euclidean distance from the current point to the given point.
 func (p Point[T]) DistanceTo(other Point[T]) float64 {
-	return other.Sub(p).Length()
+	return other.Subtract(p).Length()
 }
 
 // DistanceToSquared return euclidean distance squared (for faster comparison) from the current point to the given point.
 func (p Point[T]) DistanceToSquared(other Point[T]) T {
-	return other.Sub(p).LengthSquared()
+	return other.Subtract(p).LengthSquared()
 }
 
 // AngleTo return angle between the current point to the given point.
 func (p Point[T]) AngleTo(other Point[T]) float64 {
-	return other.Sub(p).Angle()
+	return other.Subtract(p).Angle()
 }
 
 // Equal checks for equal X and Y values with given point.
@@ -57,7 +87,6 @@ func (p Point[T]) Equal(other Point[T]) bool {
 
 // Zero checks if X and Y values are 0.
 func (p Point[T]) Zero() bool {
-	// return Equal(p.X, 0) && Equal(p.Y, 0)
 	return p.Equal(Point[T]{})
 }
 
--- point_test.go
+++ point_test.go
@@ -2,81 +2,98 @@ package geom
 
 import (
 	"encoding/json"
-	"github.com/gravitton/assert"
 	"math"
 	"testing"
+
+	"github.com/gravitton/assert"
 )
 
 func TestPoint_New(t *testing.T) {
-	testPoint(t, Pt(10, 16), 10, 16)
-	testPoint(t, Pt[float64](0.16, 204), 0.16, 204.0)
+	testPoint(t, P(10, 16), 10, 16)
+	testPoint(t, P[float64](0.16, 204), 0.16, 204.0)
 
 	testPoint(t, ZeroPoint[int](), 0, 0)
 }
 
 func TestPoint_Add(t *testing.T) {
-	testPoint(t, Pt(1, 2).Add(Vec(3, -2)), 4, 0)
-	testPoint(t, Pt(0.4, -0.25).Add(Vec(100.1, -0.1)), 100.5, -0.35)
+	testPoint(t, P(1, 2).Add(Vec(3, -2)), 4, 0)
+	testPoint(t, P(1, 2).AddXY(3, -2), 4, 0)
+	testPoint(t, P(0.4, -0.25).Add(Vec(100.1, -0.1)), 100.5, -0.35)
+	testPoint(t, P(0.4, -0.25).AddXY(100.1, -0.1), 100.5, -0.35)
 }
 
-func TestPoint_Sub(t *testing.T) {
-	testVector(t, Pt(1, 2).Sub(Pt(3, -3)), -2, 5)
-	testVector(t, Pt(0.4, -0.25).Sub(Pt(100.1, -0.1)), -99.7, -0.15)
+func TestPoint_Subtract(t *testing.T) {
+	testVector(t, P(1, 2).Subtract(P(3, -3)), -2, 5)
+	testVector(t, P(0.4, -0.25).Subtract(P(100.1, -0.1)), -99.7, -0.15)
+}
+
+func TestPoint_Multiply(t *testing.T) {
+	testPoint(t, P(1, 2).Multiply(3), 3, 6)
+	testPoint(t, P(1, 2).MultiplyXY(3, 4), 3, 8)
+	testPoint(t, P(0.4, -0.25).Multiply(-1.5), -0.6, 0.375)
+	testPoint(t, P(0.4, -0.25).MultiplyXY(-1.5, 2), -0.6, -0.5)
+}
+
+func TestPoint_Divide(t *testing.T) {
+	testPoint(t, P(5, 10).Divide(2), 3, 5)
+	testPoint(t, P(5, 10).DivideXY(3, 2), 2, 5)
+	testPoint(t, P(0.4, -0.25).Divide(-2), -0.2, 0.125)
+	testPoint(t, P(0.4, -0.25).DivideXY(-4, 0.5), -0.1, -0.5)
 }
 
 func TestPoint_Midpoint(t *testing.T) {
-	testPoint(t, Pt(1, 2).Midpoint(Pt(3, -3)), 2, -1)
-	testPoint(t, Pt(0.4, -0.25).Midpoint(Pt(100.1, -0.1)), 50.25, -0.175)
+	testPoint(t, P(1, 2).Midpoint(P(3, -3)), 2, -1)
+	testPoint(t, P(0.4, -0.25).Midpoint(P(100.1, -0.1)), 50.25, -0.175)
 }
 
 func TestPoint_Lerp(t *testing.T) {
-	testPoint(t, Pt(1, 2).Lerp(Pt(3, -3), 0.3), 2, 1)
-	testPoint(t, Pt(0.4, -0.25).Lerp(Pt(100.1, -0.1), 0.1), 10.37, -0.235)
+	testPoint(t, P(1, 2).Lerp(P(3, -3), 0.3), 2, 1)
+	testPoint(t, P(0.4, -0.25).Lerp(P(100.1, -0.1), 0.1), 10.37, -0.235)
 }
 
 func TestPoint_DistanceTo(t *testing.T) {
-	assert.EqualDelta(t, Pt(1, 2).DistanceTo(Pt(2, 3)), math.Sqrt(2), Delta)
-	assert.EqualDelta(t, Pt(0.4, -0.25).DistanceTo(Pt(0.5, -0.35)), math.Sqrt(0.02), Delta)
+	assert.EqualDelta(t, P(1, 2).DistanceTo(P(2, 3)), math.Sqrt(2), Delta)
+	assert.EqualDelta(t, P(0.4, -0.25).DistanceTo(P(0.5, -0.35)), math.Sqrt(0.02), Delta)
 }
 
 func TestPoint_DistanceToSquared(t *testing.T) {
-	assert.Equal(t, Pt(1, 2).DistanceToSquared(Pt(2, 3)), 2)
-	assert.EqualDelta(t, Pt(0.4, -0.25).DistanceToSquared(Pt(0.5, -0.35)), 0.02, Delta)
+	assert.Equal(t, P(1, 2).DistanceToSquared(P(2, 3)), 2)
+	assert.EqualDelta(t, P(0.4, -0.25).DistanceToSquared(P(0.5, -0.35)), 0.02, Delta)
 }
 
 func TestPoint_AngleTo(t *testing.T) {
-	assert.EqualDelta(t, Pt(2, 2).AngleTo(Pt(3, 2)), ToRadians(0), Delta)
-	assert.EqualDelta(t, Pt(0.4, -0.25).AngleTo(Pt(0.5, -0.35)), ToRadians(-45), Delta)
+	assert.EqualDelta(t, P(2, 2).AngleTo(P(3, 2)), ToRadians(0), Delta)
+	assert.EqualDelta(t, P(0.4, -0.25).AngleTo(P(0.5, -0.35)), ToRadians(-45), Delta)
 }
 
 func TestPoint_Equal(t *testing.T) {
-	assert.False(t, Pt(1, 2).Equal(Pt(3, -3)))
-	assert.True(t, Pt(1, 2).Equal(Pt(1, 2)))
+	assert.False(t, P(1, 2).Equal(P(3, -3)))
+	assert.True(t, P(1, 2).Equal(P(1, 2)))
 
-	assert.False(t, Pt(0.4, -0.25).Equal(Pt(100.1, -0.1)))
-	assert.True(t, Pt(0.4, -0.25).Equal(Pt(0.4, -0.25)))
-	assert.True(t, Pt(0.4, -0.25).Equal(Pt(0.4, -0.250001)))
+	assert.False(t, P(0.4, -0.25).Equal(P(100.1, -0.1)))
+	assert.True(t, P(0.4, -0.25).Equal(P(0.4, -0.25)))
+	assert.True(t, P(0.4, -0.25).Equal(P(0.4, -0.250001)))
 }
 
 func TestPoint_Zero(t *testing.T) {
-	assert.False(t, Pt(1, 2).Zero())
-	assert.True(t, Pt(0, -0).Zero())
+	assert.False(t, P(1, 2).Zero())
+	assert.True(t, P(0, -0).Zero())
 	assert.True(t, ZeroPoint[int]().Zero())
 
-	assert.False(t, Pt(0.4, -0.25).Zero())
-	assert.True(t, Pt(0.0, 0.0).Zero())
-	assert.True(t, Pt(0.0, 0.000001).Zero())
+	assert.False(t, P(0.4, -0.25).Zero())
+	assert.True(t, P(0.0, 0.0).Zero())
+	assert.True(t, P(0.0, 0.000001).Zero())
 	assert.True(t, ZeroPoint[float64]().Zero())
 }
 
 func TestPoint_String(t *testing.T) {
-	assert.Equal(t, Pt(10, 16).String(), "(+10,+16)")
-	assert.Equal(t, Pt(100, -34.0000115).String(), "(+100,-34.00)")
+	assert.Equal(t, P(10, 16).String(), "(10,16)")
+	assert.Equal(t, P(100, -34.0000115).String(), "(100,-34.00)")
 }
 
 func TestPoint_Marshall(t *testing.T) {
-	assert.JSON(t, Pt(10, 16), `{"x":10,"y":16}`)
-	assert.JSON(t, Pt(100, -34.0000115), `{"x":100.0,"y":-34.0000115}`)
+	assert.JSON(t, P(10, 16), `{"x":10,"y":16}`)
+	assert.JSON(t, P(100, -34.0000115), `{"x":100.0,"y":-34.0000115}`)
 }
 
 func TestPoint_Unmarshall(t *testing.T) {
@@ -90,12 +107,16 @@ func TestPoint_Unmarshall(t *testing.T) {
 }
 
 func TestPoint_Immutable(t *testing.T) {
-	p1 := Pt(1, 2)
-	p2 := Pt(3, -3)
+	p1 := P(1, 2)
+	p2 := P(3, -3)
 
 	p1.Add(Vec(3, -2))
-	p1.Sub(p2)
-	p1.Midpoint(p2)
+	p1.AddXY(2, 3)
+	p1.Subtract(p2)
+	p1.Multiply(2)
+	p1.MultiplyXY(3, 4)
+	p1.Divide(5)
+	p1.DivideXY(10, 100)
 	p1.Lerp(p2, 0.1)
 
 	testPoint(t, p1, 1, 2)
