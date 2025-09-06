0f39452479bcce3afa0f52d906c65a18bf84dc70
Tomáš Novotný <tomas.novotny.301@gmail.com>
2025-09-06 22:24:24 +0200

    Add generics-based constructors for geometric types in `ints` and `floats` packages

--- types/floats/types.go
+++ types/floats/types.go
@@ -14,24 +14,48 @@ type Rectangle = geom.Rectangle[float64]
 type Polygon = geom.Polygon[float64]
 type RegularPolygon = geom.RegularPolygon[float64]
 
+func P[T geom.Number](x, y T) Point {
+	return Point{X: float64(x), Y: float64(y)}
+}
+
 func ToPoint[T geom.Number](point geom.Point[T]) Point {
 	return Point{X: float64(point.X), Y: float64(point.Y)}
+}
+
+func V[T geom.Number](x, y T) Vector {
+	return Vector{X: float64(x), Y: float64(y)}
 }
 
 func ToVector[T geom.Number](vector geom.Vector[T]) Vector {
 	return Vector{X: float64(vector.X), Y: float64(vector.Y)}
 }
 
+func S[T geom.Number](width, height T) Size {
+	return Size{Width: float64(width), Height: float64(height)}
+}
+
 func ToSize[T geom.Number](size geom.Size[T]) Size {
 	return Size{Width: float64(size.Width), Height: float64(size.Height)}
+}
+
+func C[T geom.Number](center geom.Point[T], radius T) Circle {
+	return Circle{Center: ToPoint(center), Radius: float64(radius)}
 }
 
 func ToCircle[T geom.Number](circle geom.Circle[T]) Circle {
 	return Circle{Center: ToPoint(circle.Center), Radius: float64(circle.Radius)}
 }
 
+func L[T geom.Number](start, end geom.Point[T]) Line {
+	return Line{Start: ToPoint(start), End: ToPoint(end)}
+}
+
 func ToLine[T geom.Number](line geom.Line[T]) Line {
 	return Line{Start: ToPoint(line.Start), End: ToPoint(line.End)}
+}
+
+func R[T geom.Number](center geom.Point[T], size geom.Size[T]) Rectangle {
+	return Rectangle{Center: ToPoint(center), Size: ToSize(size)}
 }
 
 func ToRectangle[T geom.Number](rectangle geom.Rectangle[T]) Rectangle {
--- types/ints/types.go
+++ types/ints/types.go
@@ -14,24 +14,48 @@ type Rectangle = geom.Rectangle[int]
 type Polygon = geom.Polygon[int]
 type RegularPolygon = geom.RegularPolygon[int]
 
+func P[T geom.Number](x, y T) Point {
+	return Point{X: geom.Cast[int](float64(x)), Y: geom.Cast[int](float64(y))}
+}
+
 func ToPoint[T geom.Number](point geom.Point[T]) Point {
 	return Point{X: geom.Cast[int](float64(point.X)), Y: geom.Cast[int](float64(point.Y))}
+}
+
+func V[T geom.Number](x, y T) Vector {
+	return Vector{X: geom.Cast[int](float64(x)), Y: geom.Cast[int](float64(y))}
 }
 
 func ToVector[T geom.Number](vector geom.Vector[T]) Vector {
 	return Vector{X: geom.Cast[int](float64(vector.X)), Y: geom.Cast[int](float64(vector.Y))}
 }
 
+func S[T geom.Number](width, height T) Size {
+	return Size{Width: geom.Cast[int](float64(width)), Height: geom.Cast[int](float64(height))}
+}
+
 func ToSize[T geom.Number](size geom.Size[T]) Size {
 	return Size{Width: geom.Cast[int](float64(size.Width)), Height: geom.Cast[int](float64(size.Height))}
+}
+
+func C[T geom.Number](center geom.Point[T], radius T) Circle {
+	return Circle{Center: ToPoint(center), Radius: geom.Cast[int](float64(radius))}
 }
 
 func ToCircle[T geom.Number](circle geom.Circle[T]) Circle {
 	return Circle{Center: ToPoint(circle.Center), Radius: geom.Cast[int](float64(circle.Radius))}
 }
 
+func L[T geom.Number](start, end geom.Point[T]) Line {
+	return Line{Start: ToPoint(start), End: ToPoint(end)}
+}
+
 func ToLine[T geom.Number](line geom.Line[T]) Line {
 	return Line{Start: ToPoint(line.Start), End: ToPoint(line.End)}
+}
+
+func R[T geom.Number](center geom.Point[T], size geom.Size[T]) Rectangle {
+	return Rectangle{Center: ToPoint(center), Size: ToSize(size)}
 }
 
 func ToRectangle[T geom.Number](rectangle geom.Rectangle[T]) Rectangle {
